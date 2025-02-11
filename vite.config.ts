ecbe3323a19eaa18bd01f241947f65a1b8b6b9a9
Memet Zx <zxce3@gnuweeb.org>
2025-02-11 21:12:40 +0700

    Update TypeScript configuration and Vite setup for improved module resolution and worker support

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,19 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { fileURLToPath } from 'url';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+    plugins: [sveltekit()],
+    resolve: {
+        alias: {
+            '$workers': fileURLToPath(new URL('./src/lib/workers', import.meta.url))
+        }
+    },
+    server: {
+        host: true,
+        // allowedHosts: ['0838-182-4-100-96.ngrok-free.app'],
+    },
+    worker: {
+        format: 'es',
+    }
 });
